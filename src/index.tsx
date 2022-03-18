6165d88bd1594d4fb72a6b4fe6a5df6c93ed6cf8
Maksim Yudzeshka <yudzeshka@gmail.com>
2022-03-18 11:22:02 +0300

    deploy

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.scss";
 import { App } from "../src/components/App";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.render(
-  <Router>
+  <HashRouter>
     <App />
-  </Router>,
+  </HashRouter>,
   document.getElementById("root")
 );
